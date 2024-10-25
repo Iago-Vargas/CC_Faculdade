6fd87bdc90b5baac4f253b8b1710151622a867d8
Iago-Vargas <166236052+Iago-Vargas@users.noreply.github.com>
2024-10-24 21:11:37 -0300

    Update BancoDeDados.sql

--- Laboratório de Desenvolvimento de Software/BancoDeDados.sql
+++ Laboratório de Desenvolvimento de Software/BancoDeDados.sql
@@ -1,4 +1,4 @@
-
+-- Aula!
 CREATE TABLE veiculo (
     id INT AUTO_INCREMENT PRIMARY KEY,
     modelo VARCHAR(30),
@@ -6,3 +6,12 @@ CREATE TABLE veiculo (
     id_pessoa SMALLINT NOT NULL, -- Tipo de dado ajustado para SMALLINT
     FOREIGN KEY (id_pessoa) REFERENCES pessoa(id)
 );
+-- Casa!
+CREATE TABLE Disciplina (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    carga_horaria INT NOT NULL,
+    professor_id INT,
+    FOREIGN KEY (professor_id) REFERENCES Professores(id)
+);
+
